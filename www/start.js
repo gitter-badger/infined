c819150369527230d0addc089020ea1090b97078
Sigfrido Rodríguez Santos <identy@outlook.com>
2015-07-11 20:32:54 +0100

    .

--- www/start.js
+++ www/start.js
@@ -86,5 +86,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  debug(' \n listening on ' + bind);
+  debug(' listening on ' + bind);
 }
